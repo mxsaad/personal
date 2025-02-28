d11a8e4610016d99e41a630fe583179fdbac5ae7
Muhammad Saad <github.5s32y@slmail.me>
2025-02-28 17:16:34 -0500

    Generate import map

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,5 @@
-import * as migration_20250227_171453_initial from './20250227_171453_initial'
+import * as migration_20250227_171453_initial from './20250227_171453_initial';
+import * as migration_20250228_213620 from './20250228_213620';
 
 export const migrations = [
   {
@@ -6,4 +7,9 @@ export const migrations = [
     down: migration_20250227_171453_initial.down,
     name: '20250227_171453_initial',
   },
-]
+  {
+    up: migration_20250228_213620.up,
+    down: migration_20250228_213620.down,
+    name: '20250228_213620'
+  },
+];
